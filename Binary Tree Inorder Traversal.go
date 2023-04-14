bf96759b2cfa8c0022203b2e61c2bdb15fafa53a
Habibur Rahman <habibur.sust@gmail.com>
2023-04-15 04:00:52 +0600

    Add solution for binary tree inorder traversal

---
+++ Binary Tree Inorder Traversal.go
@@ -0,0 +1,21 @@
+package main
+
+// Problem Link: https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+func inorderTraversal(tree *TreeNode) []int {
+	result := []int{}
+
+	var inorder func(*TreeNode)
+
+	inorder = func(root *TreeNode) {
+		if root == nil { return }
+
+		inorder(root.Left)
+		result = append(result, root.Val)
+		inorder(root.Right)
+	}
+
+	inorder(tree)
+
+	return result
+}
